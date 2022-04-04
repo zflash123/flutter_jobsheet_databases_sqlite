41def334577381fc292dcf0b50f43177edcd00fa
Zaed Abdullah <z4ed.thalib123@gmail.com>
2022-04-05 05:42:52 +0700

    Fix home_page.dart

--- lib/models/item.dart
+++ lib/models/item.dart
@@ -2,33 +2,23 @@ class Item {
   int _id;
   String _name;
   int _price;
-  int _stok;
-  String _kode;
 
   int get id => _id;
 
   String get name => _name;
   set name(String value) => _name = value;
 
-  get stok => _stok;
-  set stok(value) => _stok = value;
-
-  String get kode => _kode;
-  set kode(String value) => _kode = value;
-
   get price => price;
   set price(value) => _price = value;
 
   // konstruktor versi 1
-  Item(this._name, this._price, this._stok, this._kode);
+  Item(this._name, this._price);
 
   // konstruktor versi 2: konversi dari Map ke Item
   Item.fromMap(Map<String, dynamic> map) {
     _id = map['id'];
     _name = map['name'];
     _price = map['price'];
-    _stok = map['stok'];
-    _kode = map['kode'];
   }
 
   // konversi dari Item ke Map
@@ -37,8 +27,6 @@ class Item {
     map['id'] = _id;
     map['name'] = name;
     map['price'] = price;
-    map['stok'] = stok;
-    map['kode'] = kode;
     return map;
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,51 +1,119 @@
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
 import '../models/item.dart';
+import '../dbhelper.dart';
+import 'entry_form.dart';
 
-class HomePage extends StatelessWidget{
-  HomePage({Key key}) : super(key: key);
-  final List<Item> items = [
-    Item(item_name: 'Gulaku 1 kg', price: 12500, image: 'assets/images/gulaku.jpg'),
-    Item(name: 'Garam Cap Kapal', price: 2000, image:'assets/images/garam.jpg'),
-    Item(name: 'Royco Sapi', price: 500, image:'assets/images/RoycoSapi.png'),
-    Item(name: 'Royco Ayam', price: 500, image:'assets/images/RoycoAyam.png'),
-  ];
+class Home extends StatefulWidget {
+  @override
+  HomeState createState() => HomeState();
+}
 
+class HomeState extends State<Home> {
+  DBHelper dbHelper = DBHelper();
+  int count = 0;
+  List<Item> itemList;
   @override
   Widget build(BuildContext context) {
+    updateListView();
+
+    if (itemList == null) {
+      itemList = <Item>[];
+    }
     return Scaffold(
       appBar: AppBar(
-        title: const Text("List Belanjaan"),
+        title: Text('Daftar Item'),
       ),
-      body: Container(
-        margin: EdgeInsets.all(8),
-        child: ListView.builder(
-          itemCount: items.length,
-          itemBuilder: (context, index){
-            final item = items[index];
-            return InkWell(
-              onTap: () {
-                Navigator.pushNamed(context, '/item', arguments: item);
-              },
-              child: Card(
-                child: Container(
-                  margin: EdgeInsets.all(8),
-                  child: Row(
-                    children: [
-                      Expanded(child: Text(item.name)),
-                      Expanded(
-                        child: Text(
-                          item.price.toString(),
-                          textAlign: TextAlign.end,
-                        )
-                      )
-                    ]
-                  ),
-                ),
-              ),
-            );
-          }
+      body: Column(children: [
+        Expanded(
+          child: createListView(),
         ),
-      )
+        Container(
+          alignment: Alignment.bottomCenter,
+          child: SizedBox(
+            width: double.infinity,
+            child: RaisedButton(
+                child: Text("Tambah Item"),
+                onPressed: () async {
+                  var item = await navigateToEntryForm(context, null);
+                  if (item != null) {
+                    //TODO 2 Panggil Fungsi untuk Insert ke DB
+                    int result = await dbHelper.insert(item);
+                    if (result > 0) {
+                      updateListView();
+                    }
+                  }
+                }),
+          ),
+        ),
+      ]),
     );
+  }
+
+  Future<Item> navigateToEntryForm(BuildContext context, Item item) async {
+    var result = await Navigator.push(context,
+        MaterialPageRoute(builder: (BuildContext context) {
+      return EntryForm(item);
+    }));
+    return result;
+  }
+
+  ListView createListView() {
+    TextStyle textStyle = Theme.of(context).textTheme.headline5;
+    return ListView.builder(
+      itemCount: count,
+      itemBuilder: (BuildContext context, int index) {
+        return Card(
+          color: Colors.white,
+          elevation: 2.0,
+          child: ListTile(
+            isThreeLine: true,
+            leading: CircleAvatar(
+              backgroundColor: Colors.red,
+              child: Icon(Icons.ad_units),
+            ),
+            title: Text(
+              this.itemList[index].name,
+              style: textStyle,
+            ),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Price : " + this.itemList[index].price.toString()),
+                SizedBox(
+                  height: 5.0,
+                ),                
+              ],
+            ),
+            trailing: GestureDetector(
+              child: Icon(Icons.delete),
+              onTap: () async {
+                //TODO 3 Panggil Fungsi untuk Delete dari DB berdasarkan Item
+              },
+            ),
+            onTap: () async {
+              var item =
+                  await navigateToEntryForm(context, this.itemList[index]);
+              //TODO 4 Panggil Fungsi untuk Edit data
+            },
+          ),
+        );
+      },
+    );
+  }
+
+  //update List item
+  void updateListView() {
+    final Future<Database> dbFuture = dbHelper.initDb();
+    dbFuture.then((database) {
+      //TODO 1 Select data dari DB
+      Future<List<Item>> itemListFuture = dbHelper.getItemList();
+      itemListFuture.then((itemList) {
+        setState(() {
+          this.itemList = itemList;
+          this.count = itemList.length;
+        });
+      });
+    });
   }
 }
